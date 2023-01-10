4d026f846426eeff5b4767804e69d0e5ecf68fbf
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-10 08:41:50 +0600

    file location added

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/iamtonmoy0/slack-bot-file-uploader
 
 go 1.19
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.12.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4641079431152-4614465663061-66dyDZ2CwWUD7DaZLbeIsCbv")
+	os.Setenv("CHANNEL_ID", "C04J7QZ8A2Y")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"cheat-sheet.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s,URL: %s\n", file.Name, file.URL)
+	}
 }
